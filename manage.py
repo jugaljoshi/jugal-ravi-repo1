92a6d6697e92048e3256a4e30a5c613c93a7c588
Jugal Joshi <jugal.joshi1735@gmail.com>
2016-10-04 15:26:23 +0530

    Setting path and root dir to manage.py file

--- manage.py
+++ manage.py
@@ -3,7 +3,14 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "visitorManagement.settings")
+    ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    APP_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'visitorManagement'))
+
+    sys.path.append(ROOT_DIR)
+    sys.path.append(APP_DIR)
+    app_basename = os.path.basename(APP_DIR)
+    os.environ['DJANGO_SETTINGS_MODULE'] = app_basename + '.settings'
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "visitorManagement.settings")
 
     from django.core.management import execute_from_command_line
 
